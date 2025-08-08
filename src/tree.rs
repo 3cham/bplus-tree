fd3d91aa2fd8b4becd741008b87565775e2348f1
Tung Dang <tung.dang@otto.de>
2025-08-08 08:29:45 +0200

    improve get() idiomatically & performance

--- src/tree.rs
+++ src/tree.rs
@@ -1,6 +1,6 @@
 pub struct Node<Any> {
     key: u32,
-    childrens: Vec<Node<Any>>,
+    children: Vec<Node<Any>>,
     values: Vec<Kv<Any>>
 }
 
@@ -17,7 +17,7 @@ impl<Any> Node<Any> {
             value: value
         });
 
-        Node { key: key, childrens: Vec::new(), values: values }
+        Node { key: key, children: Vec::new(), values: values }
     }
 
     // pub fn insert(&mut self, key: u32, value: Any) -> bool {
@@ -33,22 +33,18 @@ impl<Any> Node<Any> {
     // }
 
     pub fn get(&self, key: u32) -> Option<&Any> {
-        let node = &self;
-        if node.values.is_empty() {
-            for i in 1..node.childrens.len() {
-                if node.childrens[i].key > key {
-                    return node.childrens[i - 1].get(key)
-                }
+        if self.values.is_empty() {
+            // find the first child whose key is greater than the search key
+            // if not found, default to the first child
+            let idx = self.children.iter().position(|child| child.key > key);
+            match idx {
+                Some(i) if i > 0 => self.children[i - 1].get(key),
+                _ => self.children.get(0).and_then(|child| child.get(key)),
             }
-            return node.childrens.get(0).and_then(|child| child.get(key));
         } else {
-            for i in 0..node.values.len() {
-                if node.values[i].key == key {
-                    let kv = &node.values[i];
-                    return Some(&kv.value)
-                }
-            }
-            return None
+            self.values.iter()
+                .find(|kv| kv.key == key)
+                .map(|kv| &kv.value)
         }
     }
 
