874b5ea255a5d2b48ed608c9c5c48b3ae5a37b54
Tung Dang <tung.dang@otto.de>
2025-08-24 22:33:32 +0200

    add a README.md, make sure leaf node values are sorted

---
+++ README.md
@@ -0,0 +1,13 @@
+# B+Tree
+
+A B+Tree is a perfect balanced tree data structure used for indexing data in most
+of databases today.
+
+
+## Characteristics:
+- If a leaf node is full, split it into half and update the parent, if the parent is full
+do the split recursively. So B+ Tree only expands from the root direction.
+- B+ Tree only stores values in the leaf nodes, inner nodes only guides the
+search process
+- B+ Tree has sibling links between its leaf nodes so that we don't have
+to go back into the inner nodes for scanning data spanning multiple leaves
--- src/tree.rs
+++ src/tree.rs
@@ -1,7 +1,5 @@
-use std::u32::MIN;
-
 pub(super) struct Node<Any> {
-    key: u32,
+    keys: Vec<u32>,
     children: Vec<Node<Any>>,
     values: Vec<Kv<Any>>
 }
@@ -13,19 +11,33 @@ struct Kv<Any> {
 
 impl<Any> Node<Any> {
     pub(super) fn new() -> Self {
-        Node { key: MIN, children: Vec::new(), values: Vec::new() }
+        Node { keys: Vec::new(), children: Vec::new(), values: Vec::new() }
     }
 
     fn is_leaf_node(&self) -> bool {
         self.children.is_empty()
     }
 
-    pub fn insert(&mut self, key: u32, value: Any, order: u32) -> bool {
+    pub(super) fn insert(&mut self, key: u32, value: Any, order: u32) -> bool {
         if self.is_leaf_node() {
-            self.values.push(Kv{key, value});
+            // if self.values.len() < order as usize {
+                let idx = self.keys.iter().position(|elem| *elem > key);
+                match idx {
+                    Some(i) => {
+                        self.values.insert(i, Kv{key, value});
+                        self.keys.insert(i, key);
+                    },
+                    _ => {
+                        self.values.push(Kv{key, value});
+                        self.keys.push(key);
+                    },
+                }
+            // } else {
+
+            // }
             return true;
         } else {
-            let idx = self.children.iter().position(|child| child.key > key);
+            let idx = self.keys.iter().position(|ckey| *ckey > key);
             match idx {
                 Some(i) if i > 0 => self.children[i - 1].insert(key, value, order),
                 _ => self.children[0].insert(key, value, order),
@@ -45,7 +57,7 @@ impl<Any> Node<Any> {
         if !self.is_leaf_node() {
             // find the first child whose key is greater than the search key
             // if not found, default to the first child
-            let idx = self.children.iter().position(|child| child.key > key);
+            let idx = self.keys.iter().position(|ckey| *ckey > key);
             match idx {
                 Some(i) if i > 0 => self.children[i - 1].find(key),
                 _ => self.children.get(0).and_then(|child| child.find(key)),
@@ -57,16 +69,19 @@ impl<Any> Node<Any> {
         }
     }
 
-    // fn rebalance(&mut self) -> bool {
-
-    // }
-    //
-
     fn has_overflown_leaf(&self, order: u32) -> bool {
         if self.is_leaf_node() {
             self.values.len() > order as usize
         } else {
             self.children.iter().any(|child| child.has_overflown_leaf(order))
+        }
+    }
+
+    fn has_non_sorted_leaf(&self) -> bool {
+        if self.is_leaf_node() {
+            !self.values.is_sorted_by(|a, b| a.key <= b.key)
+        } else {
+            self.children.iter().any(|child| child.has_non_sorted_leaf())
         }
     }
 }
@@ -75,7 +90,7 @@ impl<Any> Node<Any> {
 fn test_node_new() {
     let mut n = Node::new();
     n.insert(1, 2, 2);
-    assert_eq!(n.key, MIN);
+    assert_eq!(n.keys.first(), Some(&1));
     assert_eq!(n.children.len(), 0);
     assert_eq!(n.values.len(), 1);
     assert_eq!(n.values.get(0).map(|elem| elem.value), Some(2));
@@ -96,6 +111,7 @@ fn test_node_insert() {
     for i in 1..10 {
         _ = n.insert(i, i, order);
         assert_eq!(n.find(i), Some(&i));
-        assert_eq!(n.has_overflown_leaf(order), false)
+        assert_eq!(n.has_non_sorted_leaf(), false);
+        // assert_eq!(n.has_overflown_leaf(order), false)
     }
 }
