232d1812e868e1a5816247cab0ac0e27463c0dad
Tung Dang <tung.dang@otto.de>
2025-09-03 21:49:43 +0200

    add test to compare with stdcollection implementation

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,4 +1,6 @@
+use std::collections::BTreeMap;
 use bplus_tree::BTree;
+use rand::random;
 
 #[test]
 fn test_new() {
@@ -29,4 +31,20 @@ fn test_modification() {
     assert_eq!(t.get(1), Some(&"11"));
 
     assert_eq!(t.update(2, "11"), false);
+}
+
+#[test]
+fn test_compare_with_btreemap() {
+    let mut t = BTree::<u32>::new();
+    let mut bm = BTreeMap::<u32, u32>::new();
+
+    for _ in [0..10e6 as i32] {
+        let (k, v) = (random::<u32>(), random::<u32>());
+        t.insert(k, v);
+        bm.insert(k, v);
+    }
+
+    for key in bm.keys() {
+        assert_eq!(bm.get(key), t.get(*key));
+    }
 }
